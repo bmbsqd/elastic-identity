2278145b9ec935b88e6c3e8903fe8b7ec3f634c5
Jonas Frost <j@duplo.org>
2014-03-21 14:49:40 -0700

    Preparing for v2, updated licenses

--- src/Bmbsqd.ElasticIdentity/ElasticUserStore.cs
+++ src/Bmbsqd.ElasticIdentity/ElasticUserStore.cs
@@ -1,9 +1,8 @@
 ﻿#region MIT License
-
 /*
 	The MIT License (MIT)
 
-	Copyright (c) 2013 Bombsquad AB
+	Copyright (c) 2013 Bombsquad Inc
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy of
 	this software and associated documentation files (the "Software"), to deal in
@@ -22,7 +21,6 @@
 	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 	CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-
 #endregion
 
 using System;
